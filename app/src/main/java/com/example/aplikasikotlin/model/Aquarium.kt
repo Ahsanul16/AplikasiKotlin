888345c71ff7fcda19dd1ed8cf7ab7d78e8e2c84
Vava-Ahsanul_16 <vavaahsanul9@gmail.com>
2024-03-04 11:22:10 +0700

    latihan class

---
+++ app/src/main/java/com/example/aplikasikotlin/model/Aquarium.kt
@@ -0,0 +1,13 @@
+package com.example.aplikasikotlin.model
+
+class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
+    // Dimensions in cm
+    var length: Int = length
+    var width: Int = width
+    var height: Int = height
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+}
